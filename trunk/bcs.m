64d90c62c95c32de686523744cf5712ce5ef01a6
david.huard <david.huard@02639219-ab21-0410-aed8-758928e7e07a>
2006-11-07 17:06:28 +0000

    Initial commit of first files

---
+++ trunk/bcs.m
@@ -0,0 +1,19 @@
+function p = bcs(types, data, bounds)
+% function probability = bcs(types, data, bounds)
+%
+% Function that returns the probability of copula models in types, 
+% given a vector of bivariate empirical cdf. 
+%
+% INPUTS:   type is one of the list {'ind' 'gumbel' 'clayton' 'sim' 'frank' 'gb' 'amh' 'joe'}
+%           data is the matrix (u,v), the empirical cdf.
+%           bounds: integration boundary on Kendall's tau. 
+%	     
+% OUTPUTS:  The probability of each copula model.
+
+
+	for i=1:length(types)	
+	    [tau_min,tau_max] = int_tau(char(types{i}));
+	    parmin = max(bounds(0), tau_min)
+	    parmax = min(bounds(1), tau_max)
+	    p(i) = quadg('posterior',parmin,parmax,1e-3,[0,128],data,char(types{i}));
+	end
