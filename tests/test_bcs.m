83e27a74989e9ce1df12e7abecd941d1ab7d6bad
david.huard <david.huard@02639219-ab21-0410-aed8-758928e7e07a>
2006-12-13 01:06:54 +0000

    Added some comments, polished test_bcs.

--- tests/test_bcs.m
+++ tests/test_bcs.m
@@ -7,11 +7,12 @@ family_generated = {'gaussian', 'gumbel' 'clayton' 'fgm' 'frank' 'amh' 'arch12',
 family_tested = {'gaussian', 'gumbel' 'clayton' 'fgm' 'frank' 'amh' 'arch12', 'arch14', 'ind'};
 
 % Number of trials
-T=1;
+T=5;
 
 % Number of samples
 N = 100;
 
+warning off MATLAB:fzero:UndeterminedSyntax
 for i=1:length(family_generated)
     f = family_generated{i};
     
@@ -25,7 +26,24 @@ for i=1:length(family_generated)
         U = copularnd(f, alpha, N);
         
         % Compute weight of each copula family.
-        p(i, :, k)  = bcs(family_tested, U, [0.01, .98]);
+        p(i, :, k)  = bcs(family_tested, U, [0.01, .97]);
+        P(i,:,k) = p(i,:,k)/sum(p(i,:,k));
     end
 end
 
+success = zeros(length(family_generated),1);
+% Count the ratio of successful identification
+for i=1:length(family_generated)
+    for k=1:T
+        [Y,I] = max(p(i, :, k));
+        if (I == i)
+            success(i)  = success(i) + 1;
+        end
+    end
+end
+
+fprintf('%-19s%10s\n', 'Family', 'Number of successful identifications')
+fprintf('-------------------------------------------------------\n')
+for i=1:length(family_generated)
+    fprintf('%-10s%10d/%d\n', family_generated{i}, success(i), T)
+end
